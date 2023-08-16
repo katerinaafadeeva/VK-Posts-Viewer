157c7adc51361d3f22480ac0e2b5919267c5df27
Katerina Fadeeva <109800319+katerinaafadeeva@users.noreply.github.com>
2023-08-16 23:40:14 +0300

    Update Post.ts

--- posts-viewer/src/types/Post.ts
+++ posts-viewer/src/types/Post.ts
@@ -10,17 +10,4 @@ export type PostTableProps = {
   post: Post;
 };
 
-// export type PostProps = {
-//   post: {
-//     id: number;
-//     text: string;
-//     date: number;
-//     views: { count: number };
-//     likes: { count: number };
-//     reposts: { count: number };
-//   };
-// };
 
-// export type PostTableProps = {
-//   posts: Post[];
-// };
